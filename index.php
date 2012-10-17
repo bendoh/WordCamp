1f7b606ea0fff5714cdc0d1b90006951203a711e
Ben Doherty <bendohmv@gmail.com>
2012-10-16 23:01:02 -0400

    Added index

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>WordCamp!</title>
+</head>
+<body>
+	<h1>WordCamp.</h1>
+
+	<p><a href="wcnyc-2012-ben-cli">WordCamp NYC 2012: WordPress on the Command-Line: It's not so scary!</a></p>
+
+	<p><a href="wcphl-2012-ben-cli">WordCamp Philly 2012: WordPress on the Command-Line: Explore, Interact, Automate</a></p>
+
+
+</body>
+</html>
